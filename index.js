d7516edca296d43ed347d679fefa182febf7ec59
Mykyta Krumalenko <prikol769@gmail.com>
2024-07-18 11:06:37 -0500

    setup

---
+++ index.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const app = express();
+
+require("dotenv").config();
+
+const PORT = process.env.PORT;
+
+app.get("/", (req, res) => {
+  res.send("Home");
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port: ${PORT}`);
+});
