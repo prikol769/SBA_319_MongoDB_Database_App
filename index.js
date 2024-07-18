456650affac2a7bbfc9bf3c1f1544aa9aeda80c5
Mykyta Krumalenko <prikol769@gmail.com>
2024-07-18 11:50:01 -0500

    added setup/users route

--- index.js
+++ index.js
@@ -1,14 +1,29 @@
 const express = require("express");
 const conn = require("./db/conn");
 const app = express();
-const PORT = process.env.PORT;
-
 require("dotenv").config();
+
+const User = require("./models/user.model");
+const setupUsers = require("./db/setupUsers");
+
+const PORT = process.env.PORT;
 
 conn();
 
 app.get("/", (req, res) => {
   res.send("Home");
+});
+
+app.get("/setup/users", async (req, res) => {
+  try {
+    await User.deleteMany({});
+    const createdUsers = await User.create(setupUsers);
+    res.json(createdUsers);
+  } catch (error) {
+    console.log(
+      `Something went wrong with connect to the database ${error.message}`
+    );
+  }
 });
 
 app.listen(PORT, () => {
